48a509b2e0d7654f232e65445484e3b805bd17b9
kimmina0874 <134137721+kimmina0874@users.noreply.github.com>
2025-04-15 22:19:53 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,27 +1,6 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from app.routes import log
-from app.services.ai_agent import ai_tick_all
-import asyncio
-from app.routes import agents  # ← 추가
+from fastapi import FastAPI
+from app.routes import nation
 
 app = FastAPI()
-app.include_router(agents.router)
-templates = Jinja2Templates(directory="templates")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(log.router)
-
-@app.get("/", response_class=HTMLResponse)
-def index(request: Request):
-	return templates.TemplateResponse("logs.html", {"request": request})
-
-@app.on_event("startup")
-async def start_loop():
-	async def run_loop():
-		while True:
-			ai_tick_all()
-			await asyncio.sleep(1)
-	asyncio.create_task(run_loop())
+app.include_router(nation.router)
